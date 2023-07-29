81c05656526b9de7f6f7b0d30a1126533bbe2b36
rootmelo92118 <32770959+rootmelo92118@users.noreply.github.com>
2023-07-29 11:13:34 +0800

    Update doaminListGenerator.py

--- doaminListGenerator.py
+++ doaminListGenerator.py
@@ -3,15 +3,15 @@ from bs4 import BeautifulSoup
 
 soup = BeautifulSoup(requests.get("https://rpz.twnic.tw/e.html").text, "html.parser")
 exec(str(soup.find("script")).split(";")[0].split("const ")[1])
-rpz_1_0_row = ""
-rpz_1_0_AdGuardHome = ""
+twnic_rpz_1_0_row = ""
+twnic_rpz_1_0_AdGuardHome = ""
 for i in rpzdata:
     for datap in i["domains"]:
-        rpz_1_0_row += datap + "\n"
-        rpz_1_0_AdGuardHome += "||" + datap + "^\n"
-with open("rpz1.0row.txt","a") as f:
-    f.write(rpz_1_0_row)
+        twnic_rpz_1_0_row += datap + "\n"
+        twnic_rpz_1_0_AdGuardHome += "||" + datap + "^\n"
+with open("twnicRPZ1.0.txt","a") as f:
+    f.write(twnic_rpz_1_0_row)
     f.close()
-with open("rpz1.0AdGuardHome.txt","a") as f:
-    f.write(rpz_1_0_AdGuardHome)
+with open("./AdGuardHome/twnicRPZ1.0.txt","a") as f:
+    f.write(twnic_rpz_1_0_AdGuardHome)
     f.close()
