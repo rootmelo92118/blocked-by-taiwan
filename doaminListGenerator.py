3b963f0ae83c0c11dd6e12a711f211ea8ded9407
rootmelo92118 <32770959+rootmelo92118@users.noreply.github.com>
2023-07-29 11:04:37 +0800

    Update doaminListGenerator.py

--- doaminListGenerator.py
+++ doaminListGenerator.py
@@ -3,10 +3,15 @@ from bs4 import BeautifulSoup
 
 soup = BeautifulSoup(requests.get("https://rpz.twnic.tw/e.html").text, "html.parser")
 exec(str(soup.find("script")).split(";")[0].split("const ")[1])
-data = ""
+rpz_1_0_row = ""
+rpz_1_0_AdGuardHome = ""
 for i in rpzdata:
     for datap in i["domains"]:
-        data += datap + "\n"
-with open("blockedbytaiwan.txt","a") as f:
-    f.write(data)
+        rpz_1_0_row += datap + "\n"
+        rpz_1_0_AdGuardHome += "||" + datap + "^\n"
+with open("rpz1.0row.txt","a") as f:
+    f.write(rpz_1_0_row)
+    f.close()
+with open("rpz1.0AdGuardHome.txt","a") as f:
+    f.write(rpz_1_0_AdGuardHome)
     f.close()
