29de504b191f79281e1e0dee1f1c75ce32212ab6
rootmelo92118 <32770959+rootmelo92118@users.noreply.github.com>
2023-07-31 13:23:30 +0800

    Update domainListGenerator.py

--- domainListGenerator.py
+++ domainListGenerator.py
@@ -7,21 +7,18 @@ def main():
     if raw.status_code != 200:
         logger.critical("Fetch data error.")
         sys.exit(1)
-    try:
-        rpz_data = json.loads(raw.text.split('<script>')[1].split(';')[0].split('= ')[1])
-        twnic_rpz_1_0_raw = ""
-        twnic_rpz_1_0_AdGuardHome = ""
-        for i in rpz_data:
-            for datap in i["domains"]:
-                twnic_rpz_1_0_raw += datap + "\n"
-                twnic_rpz_1_0_AdGuardHome += "||" + datap + "^\n"
-        with open("twnicRPZ1.0.txt","w") as f:
-            f.write(twnic_rpz_1_0_raw)
-        with open("./AdGuardHome/twnicRPZ1.0.txt","w") as f:
-            f.write(twnic_rpz_1_0_AdGuardHome)
-    except Exception as e:
-        logger.critical(e)
-        sys.exit(1)
+
+    rpz_data = json.loads(raw.text.split('<script>')[1].split(';')[0].split('= ')[1])
+    twnic_rpz_1_0_raw = ""
+    twnic_rpz_1_0_AdGuardHome = ""
+    for i in rpz_datatttt:
+        for datap in i["domains"]:
+            twnic_rpz_1_0_raw += datap + "\n"
+            twnic_rpz_1_0_AdGuardHome += "||" + datap + "^\n"
+    with open("twnicRPZ1.0.txt","w") as f:
+        f.write(twnic_rpz_1_0_raw)
+    with open("./AdGuardHome/twnicRPZ1.0.txt","w") as f:
+        f.write(twnic_rpz_1_0_AdGuardHome)
 
 if __name__ == '__main__':
     main()
