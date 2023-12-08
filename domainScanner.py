0a49b73cc1f7e184294e4f3e6ba2d5f89a6a5cc2
rootmelo92118 <32770959+rootmelo92118@users.noreply.github.com>
2023-12-08 22:00:52 +0800

    Update domainScanner.py

--- domainScanner.py
+++ domainScanner.py
@@ -83,7 +83,7 @@ class Bun:
 
 async def main(source: str, raw: str = 'raw.txt', adguard: str = 'adguard.txt', adguard_rewrote: str = 'reworte.txt'):
     bun = Bun(raw=raw, adguard=adguard, adguard_rewrote=adguard_rewrote)
-    filterlist: List[List[str]] = split_list(await bun.get_filter_list(source))
+    filterlist: List[List[str]] = split_list(bun.get_filter_list(source))
     # tasking = []
 
     for bunch in filterlist:
